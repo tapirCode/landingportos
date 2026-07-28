d28e144b9ce7e1d30fb0ffff968585d20b6cf39a
tapirCode <tapircode@DESKTOP-L5O4IDJ.localdomain>
2026-07-28 18:09:05 +0800

    update versi mobile on laptop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const navbar = document.querySelector('nav')
+const navbar = document.querySelector('.navbar')
 const humburgerBtn = document.querySelector('.humburger-btn')
 
 humburgerBtn.addEventListener('click',()=>{
