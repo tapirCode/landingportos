3119182eb526b9d340a8c544c432ad5566dfe3c0
tapirCode <tapircode@DESKTOP-L5O4IDJ.localdomain>
2026-08-05 23:12:24 +0800

    update redesain versi mobile

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ humburgerBtn.addEventListener('click',()=>{
   navbar.classList.toggle('list-active')
   humburgerBtn.classList.toggle('humburger-btn-active')
 })
+window.addEventListener("click", (e)=>{
+  console(e.target.classList(humburgerBtn))
+})
