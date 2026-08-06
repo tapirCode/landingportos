b0fe6ce7aacb73ffd04333f810f12bf18af74de9
tapirCode <tapircode@DESKTOP-L5O4IDJ.localdomain>
2026-08-06 17:11:32 +0800

    update card toolkit

---
+++ card-toolkit.js
@@ -0,0 +1,44 @@
+export const cardToolkit=[
+    {
+        id:1,
+        img:"./images/html-logo.svg",
+        title:"Html",
+        description:"Semantic structure"
+    },
+    {
+        id:2,
+        img:"./images/javascript-logo.svg",
+        title:"Javascript",
+        description:"Interaction logic"
+    },
+    {
+        id:3,
+        img:"./images/css-logo.svg",
+        title:"CSS",
+        description:"Systems & motion"
+    },
+    {
+        id:4,
+        img:"./images/react-logo.svg",
+        title:"React",
+        description:"Component thinking"
+    },
+    {
+        id:5,
+        img:"./images/node-js-logo.svg",
+        title:"Node.js",
+        description:"Product foundations"
+    },
+    {
+        id:6,
+        img:"./images/tailwind-logo.svg",
+        title:"Talwind CSS",
+        description:"Rapid precision"
+    },
+    {
+        id:7,
+        img:"./images/expressjs-logo.svg",
+        title:"Express.js",
+        description:"Reliable APIs"
+    }
+]
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { cardToolkit } from "./card-toolkit.js"
+const cardToolkits = document.querySelector('.card-toolkits')
 const navbar = document.querySelector('.navbar')
 const humburgerBtn = document.querySelector('.humburger-btn')
 
@@ -8,3 +10,20 @@ humburgerBtn.addEventListener('click',()=>{
 window.addEventListener("click", (e)=>{
   console(e.target.classList(humburgerBtn))
 })
+
+function sanitize(text){
+  const container =document.createElement('div')
+  container.textContent=text
+  return container.innerHTML
+}
+
+cardToolkit.forEach(({id, img, title, description})=>{
+  cardToolkits.insertAdjacentHTML("beforeend",
+    `<div class="card1">
+    <img src="${img}">
+    <h4>${sanitize(title)}</h4>
+    <p>${sanitize(description)}</p>
+    </div>
+    `
+    )
+    })
